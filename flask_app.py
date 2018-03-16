e915d0a1de92f10d844645d74cadbd5551c5e9a6
OO OO <oo@home.org>
2018-03-16 22:16:02 +0000

    wip

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,33 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
-
+from flask                      import Flask
+from password                   import db_host, db_database, db_user, db_password
+from sqlalchemy                 import create_engine
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy                 import Column, Integer, String
+from sqlalchemy.orm             import sessionmaker
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from majszajt!'
 
-@app.route('/qqq')
-def f_qqq():
-    return 'from qqqq'
+@app.route('/ajemdibi_szorcs')
+def imdb_search():
+    engine = create_engine('mysql+mysqldb://' + db_user + ':' + db_password + '@' + db_host + '/' + db_database , echo = True)
+    Base = declarative_base()
+    class Movie(Base):
+        __tablename__ = 'tb_movie'
+
+        id = Column(String, primary_key = True)
+        title_primary = Column(String)
+
+    Session = sessionmaker(bind = engine)
+    session = Session()
+
+    idd = session.query(Movie.id).first()
+
+    session.close()
+
+    return idd
 
 
 
