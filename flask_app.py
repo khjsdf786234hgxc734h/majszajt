ccd14197bf5fe7c4b8f809c21a4a36e6d6554c71
OO OO <oo@home.org>
2018-07-13 23:07:57 +0000

    wip

--- flask_app.py
+++ flask_app.py
@@ -2,11 +2,13 @@ from flask                      import Flask, redirect, render_template, request
 from password                   import db_host, db_database, db_user, db_password
 from sqlalchemy                 import create_engine
 from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy                 import Column, Integer, String
+from sqlalchemy                 import Column, Integer, String, Numeric
 from sqlalchemy.orm             import sessionmaker
 from sqlalchemy.orm             import exc
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
 
 engine = create_engine('mysql+mysqldb://' + db_user + ':' + db_password + '@' + db_host + '/' + db_database , echo = True)
 
@@ -15,7 +17,18 @@ Base = declarative_base()
 class Movie(Base):
     __tablename__ = 'tb_movie'
     id = Column(String, primary_key = True)
-    title_primary = Column(String)
+    title_primary         = Column(String)
+    title_secondary       = Column(String) 
+    title_primary_ascii   = Column(String)
+    title_secondary_ascii = Column(String)
+    year                  = Column(Integer)
+    genre                 = Column(String)
+    rating                = Column(Numeric(3,1))
+    vote                  = Column(Integer)
+    country               = Column(String)
+
+
+
 
 Session = sessionmaker(bind = engine)
 
@@ -34,9 +47,11 @@ def imdb_search():
         return render_template('movie_search.html', list_movies = list_movies)
 
     try:
-        #session = Session()
         movie_title = request.form['movie_title']
-        list_movies = session.query(Movie.title_primary).filter(Movie.title_primary.ilike('%' + movie_title + '%'))
+        movie_year  = request.form['movie_year'] 
+        list_movies = session.query(Movie.title_primary, Movie.year, Movie.genre, Movie.rating, Movie.vote, Movie.country).\
+            filter(Movie.title_primary.ilike('%' + movie_title + '%')).\
+            filter(Movie.year==movie_year)
     #except exc.NoResultFound, exc.MultipleResultsFound:
     except exc.SQLAlchemyError:
 #        list_movies = []
@@ -47,7 +62,6 @@ def imdb_search():
 #        list_movies = []
 
     finally:
-#        session.close()
         pass
 
     return render_template('movie_search.html', list_movies = list_movies ) 
