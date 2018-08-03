99692b5177ba0eaa58e93ecf5214770152609d65
OO OO <oo@home.org>
2018-08-03 12:53:09 +0000

    wip

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 from flask                      import Flask, redirect, render_template, request, url_for
 from password                   import db_host, db_database, db_user, db_password
 from sqlalchemy                 import create_engine
-from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy                 import Column, Integer, String, Numeric
 from sqlalchemy.orm             import sessionmaker
 from sqlalchemy.orm             import exc
@@ -18,7 +18,7 @@ class Movie(Base):
     __tablename__ = 'tb_movie'
     id = Column(String, primary_key = True)
     title_primary         = Column(String)
-    title_secondary       = Column(String) 
+    title_secondary       = Column(String)
     title_primary_ascii   = Column(String)
     title_secondary_ascii = Column(String)
     year                  = Column(Integer)
@@ -39,16 +39,16 @@ def hello_world():
     return 'Hello from majszajt!'
 
 @app.route('/ajemdibi_szorcs', methods = ['GET', 'POST'])
-def imdb_search():
-    
+def ajemdibi_szorcs():
+
     list_movies = []
-    
+
     if request.method == 'GET':
-        return render_template('movie_search.html', list_movies = list_movies)
+        return render_template('ajemdibi_szorcs.html', list_movies = list_movies)
 
     try:
         movie_title = request.form['movie_title']
-        movie_year  = request.form['movie_year'] 
+        movie_year  = request.form['movie_year']
         list_movies = session.query(Movie.title_primary, Movie.year, Movie.genre, Movie.rating, Movie.vote, Movie.country).\
             filter(Movie.title_primary.ilike('%' + movie_title + '%')).\
             filter(Movie.year==movie_year)
@@ -56,7 +56,7 @@ def imdb_search():
     except exc.SQLAlchemyError:
 #        list_movies = []
         pass
-    
+
     else:
         pass
 #        list_movies = []
@@ -64,6 +64,6 @@ def imdb_search():
     finally:
         pass
 
-    return render_template('movie_search.html', list_movies = list_movies ) 
+    return render_template('ajemdibi_szorcs.html', list_movies = list_movies )
 
 
