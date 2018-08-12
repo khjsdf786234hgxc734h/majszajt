2e61d09c51e18f027362317b8e09d087c49c5e65
OO OO <oo@home.org>
2018-08-12 18:07:02 +0000

    wip

--- __init__.py
+++ __init__.py
@@ -1,18 +1,8 @@
 from config import db_host, db_database, db_user, db_password
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-#from flask import g
-
 
 engine = create_engine('mysql+mysqldb://' + db_user + ':' + db_password + '@' + db_host + '/' + db_database , echo = True)
 
 db_session = scoped_session(sessionmaker(bind = engine))
-
-#if not hasattr(g, 'db_session'):
-#    session = sessionmaker(bind = engine)
-#    g.db_session = session
-
-
-
-
 
